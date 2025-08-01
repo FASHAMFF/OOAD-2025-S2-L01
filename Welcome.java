a199e331cb60aee85a624bd5acb7a8379810c31b
FASHAMFF <IT24104320@my.sliit.lk>
2025-08-01 11:37:17 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Welcome{
+  public static void main (String[]args){
+    Scanner fName = new Scanner (System.in);
+    System.out.println("Enter your First Name");
+    String firstName = fName.nextLine(); 
+    Scanner lName = new Scanner (System.in);
+    System.out.println("Enter your Last Name");
+    String lastName = lName.nextLine(); 
+    System.out.println("Welcome to the second yeara" + firstName + lastName);
+}
+}
